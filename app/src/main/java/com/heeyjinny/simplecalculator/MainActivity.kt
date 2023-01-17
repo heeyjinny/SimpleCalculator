d3b373bc67913c2f8ef26b541f9a7290b5886c5b
heeyjinny <heeyjinny@gmail.com>
2023-01-17 21:59:48 +0900

    TableLayout

--- app/src/main/java/com/heeyjinny/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/heeyjinny/simplecalculator/MainActivity.kt
@@ -2,10 +2,16 @@ package com.heeyjinny.simplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }//onCreate
+
+    fun buttonClicked(v: View){
+
     }
-}
+
+}//MainActivity
